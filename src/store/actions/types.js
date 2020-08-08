de047a0e308473b6331df29d9771c5a01f162b2a
Naim Uddin <naimuddin374@gmail.com>
2020-08-08 19:21:55 +0600

    Updat

--- src/store/actions/types.js
+++ src/store/actions/types.js
@@ -1,5 +1,5 @@
-// export const API_URL = 'http://api.showtalent.xyz/'
-export const API_URL = 'http://localhost:8000/'
+export const API_URL = 'http://api.showtalent.xyz/'
+// export const API_URL = 'http://localhost:8000/'
 
 export const SET_MESSAGE = 'SET_MESSAGE'
 export const SET_HOME_DATA = 'SET_HOME_DATA'
